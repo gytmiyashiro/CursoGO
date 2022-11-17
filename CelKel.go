7cf35c684dee9cc7f1ac30b459e98bf9be1b52f3
gytmiyashiro <118420496+gytmiyashiro@users.noreply.github.com>
2022-11-17 02:10:12 -0300

    Add files via upload

---
+++ CelKel.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const ebulikelvin = 373
+
+func main() {
+
+	var celsius = ebulikelvin - 273
+
+	fmt.Println("A temperatura de ebulição da água na escala celsius é de:", celsius)
+}
